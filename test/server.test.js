f1e3e6722237133f9d67db027e675ba63be0066b
Ronin619 <eytam619@gmail.com>
2026-02-28 18:16:49 -0500

    implement test and end point for /api/brands

--- test/server.test.js
+++ test/server.test.js
@@ -1,14 +1,30 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const server = require('../app/server'); // Adjust the path as needed
-
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const server = require("../app/server"); // Adjust the path as needed
 const should = chai.should();
 chai.use(chaiHttp);
 
 // TODO: Write tests for the server
+describe("Brands", () => {
+  describe("/GET brands", () => {
+    it("it should get all sunglass brands", (done) => {
+      chai
+        .request(server)
+        .get("/api/brands")
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an("array");
+          res.body.forEach((item) => {
+            item.should.be.an("object");
+            item.should.have.property("id");
+            item.should.have.property("name");
+          });
+          done();
+        });
+    });
+  });
+});
 
-describe('Brands', () => {});
+describe("Login", () => {});
 
-describe('Login', () => {});
-
-describe('Cart', () => {});
+describe("Cart", () => {});
